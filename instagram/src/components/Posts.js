5233062fced0b194ec5422548f0b61fd1a7a25b0
Andrey <andrey.sviontek@gmail.com>
2019-05-27 16:17:41 +0500

    feat: create users list

--- instagram/src/components/Posts.js
+++ instagram/src/components/Posts.js
@@ -5,7 +5,8 @@ export default class Posts extends Component {
   render() {
     return(
       <div className="left">
-        <Post alt="Nature" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFn0ZI5qRDQd8sBkN4CCVVnwk5z_tvtwX5jm0bfCs6J7BJnq-Y4Q" />
+        <Post alt="Nature" src="http://images.france.fr/zeaejvyq9bhj/4VGVbWT4kwsIyqaIuyiYs2/69b40a00fddb2b2c26ebd472fa6e4186/nature_dordogne.jpg?w=1200&h=630&q=70&fl=progressive&fit=fill" />
+        <Post alt="Nature" src="https://www.visitportugal.com/sites/www.visitportugal.com/files/mediateca/23_660x371.jpg" />
       </div>
     )
   }
